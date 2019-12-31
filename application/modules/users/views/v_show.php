af5bf3ec7c50edcf7bc59747a9a15956a64e97a5
RobbiAbd <robbyabd10@gmail.com>
2019-12-31 08:05:20 +0700

    add ion auth

--- application/modules/users/views/v_show.php
+++
@@ -1,7 +0,0 @@
-<div class="content">
-	<h2 class="text-center">ini content</h2>
-	<p>
-		Halaman ini dibuat dengan modul <strong>HMVC Codeigniter</strong>,
-		dibuat permudah supaya memudahkan pengguna mengembangkan web nantinya.
-	</p>
-</div>
